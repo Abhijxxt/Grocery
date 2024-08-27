540cd040e01066ef9f287fdc99c6d8cf091e5b1b
Abhijeet Chatterjee <97887115+Abhijxxt@users.noreply.github.com>
2024-08-27 20:20:30 +0530

    FireStore and Authentication set up

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDztVn3utSbq0bXicfdzvdG0V_CuRp3ifg",
@@ -11,3 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig); 
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
